f3a8b9893bec9d31fd070e60f4093b6c6366f0f9
Satyam <satyamj0310btechaiml2026@kccitm.edu.in>
2023-06-15 23:42:04 +0530

    .

---
+++ gradingStudents.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int a[100],grade,n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+        if(a[i]%5>=2){
+            cout<<"anjali is my bue";
+        }
+    }
+    
+}
